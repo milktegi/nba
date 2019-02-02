ef152caa9ccaf3a81ddd4beda4e118c97fdb4833
milktegi <milkteegi@gmail.com>
2019-02-03 00:40:55 +0900

    rendered logo

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import style from './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  
+  const logo = () => {
+    return (
+      <Link to="/" className={style.logo}>
+        <img alt="nba logo" src="/images/nba_logo.png"/>
+      </Link>
+    )
+  }
+  
   return(
-    <header>
-      헤더
+    <header className={style.header}>
+      <div className={style.headerOpt}>
+        {logo()}
+      </div>
     </header>
   )
 }
