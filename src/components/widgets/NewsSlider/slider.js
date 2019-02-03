f87bfa61f196bed9bebc8a4d25352b9723c9586a
milktegi <milkteegi@gmail.com>
2019-02-04 02:36:34 +0900

    completed slider

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -5,27 +5,34 @@ import SliderTemplates from './slider_template';
 // import { URL } from '../../../config';
 
 class NewsSlider extends Component {
+  state = {
+    news: []
+  };
 
-    state = {
-        news:[]
-    }
+  componentWillMount() {
+    axios
+      .get(
+        `
+		http://localhost:3004/articles?_start=${this.props.start}
+		&_end=${this.props.amount}
+      `
+      )
+      .then((response) => {
+        this.setState({
+          news: response.data
+        });
+      });
+  }
 
-    componentWillMount(){
-        axios.get(`http://localhost:3004/articles?_start=0&_end=3
-      `)
-        .then( response => {
-            this.setState({
-                news:response.data 
-            })
-        })
-    }
-
-    render(){
-        return(
-            <SliderTemplates data={this.state.news} type="featured" />
-        )
-    }
-
+  render() {
+    return (
+      <SliderTemplates
+        settings={this.props.settings}
+        data={this.state.news}
+        type={this.props.type}
+      />
+    );
+  }
 }
 
 export default NewsSlider;
