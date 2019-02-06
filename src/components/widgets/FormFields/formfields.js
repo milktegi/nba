13f886b960ce2953fca1b8f7d8dc850573fafd71
milktegi <milkteegi@gmail.com>
2019-02-06 11:51:10 +0900

    rendered signin/up form ui

--- src/components/widgets/FormFields/formfields.js
+++ src/components/widgets/FormFields/formfields.js
@@ -2,12 +2,22 @@ import React from 'react';
 import styles from './formfields.css';
 
 const formFields = ({ id, formdata, change }) => {
+  
   const renderTemplate = () => {
     let formTemplate = null;
 
     switch (formdata.element) {
       case 'input':
-        formTemplate = <div>input</div>;
+        formTemplate = (
+          <div>
+            <input
+            {...formdata.config}
+            value={formdata.value}
+            onBlur={(event)=>change({event, id, blur: true})}
+            onChange={(event)=>change({event, id, blur: false})}
+            />
+          </div>
+        );
         break;
       default:
         formTemplate = null;
